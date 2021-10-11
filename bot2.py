3e4252c926ff222357c04f54b599646e2f298fda
quiquinho <quiquinho@gmail.com>
2021-10-11 20:27:19 +0200

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,116 +1,192 @@
-import logging, time
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Simple inline keyboard bot with multiple CallbackQueryHandlers.
+This Bot uses the Updater class to handle the bot.
+First, a few callback functions are defined as callback query handler. Then, those functions are
+passed to the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Example of a bot that uses inline keyboard that has multiple CallbackQueryHandlers arranged in a
+ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line to stop the bot.
+"""
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler
+import logging
 import os
-import random
-import sys
-import requests
-from telegram.ext import *
-from telegram import *
-from funciones import *
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Stages
+FIRST, SECOND = range(2)
+# Callback data
+ONE, TWO, THREE, FOUR = range(4)
 
 
-
-# Enabling logging
-logging.basicConfig(level=logging.INFO,
-                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger = logging.getLogger()
-
-# Getting mode, so we could define run function for local and Heroku setup
-mode = os.getenv("MODE")
-# mode = 'dev'
-TOKEN = os.getenv("TOKEN")
-if mode == "dev":
-    def run(updater):
-        updater.start_polling()
-else :
-    def run(updater):
-        PORT = int(os.environ.get("PORT", "8443"))
-        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
-        updater.start_webhook(listen="0.0.0.0",
-                              port=PORT,
-                              url_path=TOKEN)
-        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
-
-
-# FUNCIONES DEL BOT
-
-# def start(bot, update):
-#   """ This function will be executed when '/start' command is received """
-
-#   message = "Bienvenido al asistente personal! \n\n/list para ver tus opciones"
-#   bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-#   bot.send_message(chat_id=update.message.chat_id, text=message)
-
-def listar (bot, update):
-    """ This function will be executed when plain text message is received """
-    text = update.message.text
-    opciones = ''
-    for func in arrayFunciones:
-      opciones += '/'+func+'\n'
-    message = "*Comandos*\n*########*\n"+opciones
-    bot.send_message(chat_id=update.message.chat_id,
-                     parse_mode='markdown', text=message)
-
-def start (bot, update):
-    """ This function will be executed when plain text message is received """
-
-
-
-    text = update.message.text
+def start(update, context):
+    """Send message on `/start`."""
+    # Get user that sent /start and log his name
+    user = update.message.from_user
+    logger.info("User %s started the conversation.", user.first_name)
+    # Build InlineKeyboard where each button has a displayed text
+    # and a string as callback_data
+    # The keyboard is a list of button rows, where each row is in turn
+    # a list (hence `[[...]]`).
     keyboard = [
-                  [
-                    InlineKeyboardButton("🗼 Faro de Vigo", callback_data='Faro'),
-                    InlineKeyboardButton("⚽ Marca", callback_data='marca'),
-                    InlineKeyboardButton("🇪🇸 El Pais", callback_data='el_pais')
-                  ],
-                  [
-                    InlineKeyboardButton("💻 Xataka", callback_data='xataka'),
-                    InlineKeyboardButton("🐢 Microsiervos", callback_data='msv'),
-                    InlineKeyboardButton("🇬🇧 BBC", callback_data='bbc')
-                  ], 
-                  [
-                    InlineKeyboardButton("🗽 New York Times", callback_data='nyt'),
-                    InlineKeyboardButton("📘 Cole", callback_data='cole'),
-                    InlineKeyboardButton("👑 Coronavirus", callback_data='coronavirus')
-                  ]
-                ]
- 
+        [InlineKeyboardButton("1", callback_data=str(ONE)),
+         InlineKeyboardButton("2", callback_data=str(TWO))]
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard)
- 
-    update.message.reply_text('Selecciona tu medio:', reply_markup=reply_markup)    
+    # Send message with text and appended InlineKeyboard
+    update.message.reply_text(
+        "Start handler, Choose a route",
+        reply_markup=reply_markup
+    )
+    # Tell ConversationHandler that we're in state `FIRST` now
+    return FIRST
 
-def button(bot, update):
+
+def start_over(update, context):
+    """Prompt same text & keyboard as `start` does but not as new message"""
+    # Get CallbackQuery from Update
     query = update.callback_query
-    
-    print(query.data)
-    for entrada in funcion_noticias(query.data):
-      query.message.reply_text(entrada)
-    bot.send_message(chat_id=query.message.chat_id,
-                     parse_mode='markdown', text='/start')
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+    keyboard = [
+        [InlineKeyboardButton("1", callback_data=str(ONE)),
+         InlineKeyboardButton("2", callback_data=str(TWO))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    # Instead of sending a new message, edit the message that
+    # originated the CallbackQuery. This gives the feeling of an
+    # interactive menu.
+    query.edit_message_text(
+        text="Start handler, Choose a route",
+        reply_markup=reply_markup
+    )
+    return FIRST
+
+
+def one(update, context):
+    """Show new choice of buttons"""
+    query = update.callback_query
+    query.answer()
+    keyboard = [
+        [InlineKeyboardButton("3", callback_data=str(THREE)),
+         InlineKeyboardButton("4", callback_data=str(FOUR))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="First CallbackQueryHandler, Choose a route",
+        reply_markup=reply_markup
+    )
+    return FIRST
+
+
+def two(update, context):
+    """Show new choice of buttons"""
+    query = update.callback_query
+    query.answer()
+    keyboard = [
+        [InlineKeyboardButton("1", callback_data=str(ONE)),
+         InlineKeyboardButton("3", callback_data=str(THREE))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="Second CallbackQueryHandler, Choose a route",
+        reply_markup=reply_markup
+    )
+    return FIRST
+
+
+def three(update, context):
+    """Show new choice of buttons"""
+    query = update.callback_query
+    query.answer()
+    keyboard = [
+        [InlineKeyboardButton("Yes, let's do it again!", callback_data=str(ONE)),
+         InlineKeyboardButton("Nah, I've had enough ...", callback_data=str(TWO))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="Third CallbackQueryHandler. Do want to start over?",
+        reply_markup=reply_markup
+    )
+    # Transfer to conversation state `SECOND`
+    return SECOND
+
+
+def four(update, context):
+    """Show new choice of buttons"""
+    query = update.callback_query
+    query.answer()
+    keyboard = [
+        [InlineKeyboardButton("2", callback_data=str(TWO)),
+         InlineKeyboardButton("4", callback_data=str(FOUR))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="Fourth CallbackQueryHandler, Choose a route",
+        reply_markup=reply_markup
+    )
+    return FIRST
+
+
+def end(update, context):
+    """Returns `ConversationHandler.END`, which tells the
+    ConversationHandler that the conversation is over"""
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+        text="See you next time!"
+    )
+    return ConversationHandler.END
+
+
+def main():
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(os.getenv("TOKEN"), use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Setup conversation handler with the states FIRST and SECOND
+    # Use the pattern parameter to pass CallbackQueries with specific
+    # data pattern to the corresponding handlers.
+    # ^ means "start of line/string"
+    # $ means "end of line/string"
+    # So ^ABC$ will only allow 'ABC'
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            FIRST: [CallbackQueryHandler(one, pattern='^' + str(ONE) + '$'),
+                    CallbackQueryHandler(two, pattern='^' + str(TWO) + '$'),
+                    CallbackQueryHandler(three, pattern='^' + str(THREE) + '$'),
+                    CallbackQueryHandler(four, pattern='^' + str(FOUR) + '$')],
+            SECOND: [CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
+                     CallbackQueryHandler(end, pattern='^' + str(TWO) + '$')]
+        },
+        fallbacks=[CommandHandler('start', start)]
+    )
+
+    # Add ConversationHandler to dispatcher that will be used for handling
+    # updates
+    dp.add_handler(conv_handler)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 
 if __name__ == '__main__':
-    logger.info("Starting bot")
-    updater = Updater(TOKEN)
-
-    arrayFunciones = ['start','list','menu']
-
-    # Command handlers
-    start_handler = CommandHandler('start', start)
-    list_handler = CommandHandler('list', listar)
-
-
-
-    # Add the handlers to the bot
-    
-    updater.dispatcher.add_handler(start_handler)
-    updater.dispatcher.add_handler(list_handler)
-
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-
-
-
-
-run(updater)
-
-
+    main()
